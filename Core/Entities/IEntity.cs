03558b3ad47687bd31b5f07ed5ba703b92d3e5f5
Ramis Yuksel <ramisyk97@gmail.com>
2021-02-15 23:48:59 +0300

    core oluşturuldu DA ve Entity cıre  a taşındı

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //IEntity impelent ediliyorsa veritabanı tablosudur.
     public interface IEntity
