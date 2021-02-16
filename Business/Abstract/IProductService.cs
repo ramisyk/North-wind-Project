c8a61490918ae576e9ae6d861c03e4a2f1f52d19
Ramis Yuksel <ramisyk97@gmail.com>
2021-02-16 19:42:34 +0300

    result oluşturuldu

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
+        IResult Add(Product product);
         List<Product> GetAll();
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetAllByUnitPrice(decimal min, decimal max);
         List<ProductDetailDto> GetProductDetails();
+        Product GetById(int productId);
     }
 }
