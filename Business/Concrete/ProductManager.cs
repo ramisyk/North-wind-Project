71fe8a166bbb58631f34037d8d3e18f4c596eba3
Ramis Yuksel <ramisyk97@gmail.com>
2021-02-22 08:58:04 +0300

    validator hatası çözüldü -> parametre yanlış verilmiş

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -21,7 +21,7 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-        [ValidationAspect(typeof(Product))]
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             _productDal.Add(product);
